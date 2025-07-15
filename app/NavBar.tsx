a105b84f2f3e18903f870eebf0f0135c33066b9b
mgbarois <mgbarois@gmail.com>
2025-07-15 13:39:32 +0200

    style active route in nav

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,12 +1,17 @@
+"use client";
 import React from "react";
 import Link from "next/link";
 import { AiFillBug } from "react-icons/ai";
+import { usePathname } from "next/navigation";
+import classNames from "classnames";
 
 const NavBar = () => {
   const links = [
     { label: "Dashboard", href: "/" },
     { label: "Issues", href: "/issues" },
   ];
+  const currentPath = usePathname();
+  console.log(currentPath);
   return (
     <nav className="flex space-x-6 px-5 border-b mb-5 h-14 items-center">
       <Link href="/">
@@ -17,10 +22,17 @@ const NavBar = () => {
           return (
             <li key={link.href}>
               <Link
-                className="hover:opacity-50 transition-opacity"
+                // className={`
+                //  ${
+                //    currentPath === link.href ? "opacity-100" : "opacity-70"
+                //  } hover:opacity-85 transition-opacity`}
+                className={classNames({
+                  "opacity-100": link.href === currentPath,
+                  "opacity-70": link.href !== currentPath,
+                  "hover:opacity-85 transition-opacity": true,
+                })}
                 href={link.href}
               >
-                {" "}
                 {link.label}
               </Link>
             </li>
