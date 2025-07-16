a1d39b62099d79243d3973b86b8f8b8000ade4ce
mgbarois <mgbarois@gmail.com>
2025-07-16 18:01:23 +0200

    build api for creating issues

---
+++ app/api/issues/route.tsx
@@ -0,0 +1,28 @@
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+import prisma from "@/prisma/client";
+
+const createIssueSchema = z.object({
+  title: z.string().min(1).max(255),
+  description: z.string().min(1),
+});
+
+export const GET = async () => {
+  const issues = await prisma.issue.findMany();
+  return NextResponse.json(issues);
+};
+
+export const POST = async (req: NextRequest) => {
+  const body = await req.json();
+  const validation = createIssueSchema.safeParse(body);
+  if (!validation.success) {
+    return NextResponse.json(JSON.parse(validation.error.message), {
+      status: 400,
+    });
+  }
+  const { title, description } = body;
+  const newIssue = await prisma.issue.create({
+    data: { title, description },
+  });
+  return NextResponse.json(newIssue, { status: 201 });
+};
