7a0dec1e4d1914a5748d21a50f2002b894c76666
mgbarois <mgbarois@gmail.com>
2025-07-22 21:56:24 +0200

    add edit button

--- app/issues/[id]/IssueDetail.tsx
+++ app/issues/[id]/IssueDetail.tsx
@@ -1,8 +1,10 @@
 import { Skeleton } from "@/app/components";
+import Link from "next/link";
 import IssueStatusBadge from "@/app/components/IssueStatusBadge";
 import { Issue } from "@/app/generated/prisma";
-import { Box, Card, Flex, Heading } from "@radix-ui/themes";
+import { Box, Button, Card, Flex, Grid, Heading } from "@radix-ui/themes";
 import ReactMarkdown from "react-markdown";
+import { BsPencilFill } from "react-icons/bs";
 
 interface Props {
   issue?: Issue;
@@ -10,26 +12,40 @@ interface Props {
 
 const IssueDetail = ({ issue }: Props) => {
   return (
-    <Box className="max-w-xl">
-      <Heading>{issue ? issue.title : <Skeleton />}</Heading>
-      <Flex gap="2" my="2">
-        <p>
+    <Grid columns={{ initial: "1", md: "2" }} gap="5">
+      <Box className="max-w-xl">
+        <Heading>{issue ? issue.title : <Skeleton />}</Heading>
+        <Flex gap="2" my="2">
+          <p>
+            {issue ? (
+              <IssueStatusBadge status={issue.status} />
+            ) : (
+              <Skeleton size="xs" />
+            )}
+          </p>
+          <p>
+            {issue ? issue.createdAt.toDateString() : <Skeleton size="s" />}
+          </p>
+        </Flex>
+        <Card className="prose" mt="5">
           {issue ? (
-            <IssueStatusBadge status={issue.status} />
+            <ReactMarkdown>{issue.description}</ReactMarkdown>
           ) : (
-            <Skeleton size="xs" />
+            <Skeleton count={3} />
           )}
-        </p>
-        <p>{issue ? issue.createdAt.toDateString() : <Skeleton size="s" />}</p>
-      </Flex>
-      <Card className="prose" mt="5">
+        </Card>
+      </Box>
+      <Box className="max-w-xl">
         {issue ? (
-          <ReactMarkdown>{issue.description}</ReactMarkdown>
+          <Button>
+            <BsPencilFill />
+            <Link href={`/issues/${issue.id}/edit`}>Edit Issue</Link>
+          </Button>
         ) : (
-          <Skeleton count={3} />
+          <Skeleton size="s" height="2rem" />
         )}
-      </Card>
-    </Box>
+      </Box>
+    </Grid>
   );
 };
 
