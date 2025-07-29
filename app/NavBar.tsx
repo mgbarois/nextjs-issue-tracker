3afe7d82ac573aca9f01bdc44c780cb23b8dae72
mgbarois <mgbarois@gmail.com>
2025-07-29 09:18:29 +0200

    refactor: nav bar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -13,17 +13,12 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { AiFillBug } from "react-icons/ai";
 
+const links = [
+  { label: "Dashboard", href: "/" },
+  { label: "Issues", href: "/issues" },
+];
+
 const NavBar = () => {
-  const links = [
-    { label: "Dashboard", href: "/" },
-    { label: "Issues", href: "/issues" },
-  ];
-  const currentPath = usePathname();
-  const { status, data: session } = useSession();
-
-  const userName = session?.user?.name;
-  const userImage = session?.user?.image;
-
   return (
     <nav className="px-5 border-b mb-5 py-3">
       <Container>
@@ -32,59 +27,79 @@ const NavBar = () => {
             <Link href="/">
               <AiFillBug />
             </Link>
-            <ul className="flex space-x-6">
-              {links.map((link) => {
-                return (
-                  <li key={link.href}>
-                    <Link
-                      // className={`
-                      //  ${
-                      //    currentPath === link.href ? "opacity-100" : "opacity-70"
-                      //  } hover:opacity-85 transition-opacity`}
-                      className={classNames({
-                        "opacity-100": link.href === currentPath,
-                        "opacity-70": link.href !== currentPath,
-                        "hover:opacity-85 transition-opacity": true,
-                      })}
-                      href={link.href}
-                    >
-                      {link.label}
-                    </Link>
-                  </li>
-                );
-              })}
-            </ul>
+            <NavLinks />
           </Flex>
-          <Box>
-            {status === "authenticated" ? (
-              <DropdownMenu.Root>
-                <DropdownMenu.Trigger>
-                  <Avatar
-                    size="2"
-                    radius="full"
-                    variant="solid"
-                    src={userImage || undefined}
-                    fallback={userName ? userName.split(" ")[0][0] : "U"}
-                    className="cursor-pointer"
-                  />
-                </DropdownMenu.Trigger>
-                <DropdownMenu.Content>
-                  <DropdownMenu.Label>
-                    <Text>{session.user!.email}</Text>
-                  </DropdownMenu.Label>
-                  <DropdownMenu.Separator />
-                  <DropdownMenu.Item>
-                    <Link href="/api/auth/signout">Sign out</Link>
-                  </DropdownMenu.Item>
-                </DropdownMenu.Content>
-              </DropdownMenu.Root>
-            ) : (
-              <Link href="/api/auth/signin">Sign in</Link>
-            )}
-          </Box>
+          <AuthStatus />
         </Flex>
       </Container>
     </nav>
+  );
+};
+
+const NavLinks = () => {
+  const currentPath = usePathname();
+  return (
+    <ul className="flex space-x-6">
+      {links.map((link) => {
+        return (
+          <li key={link.href}>
+            <Link
+              className={classNames({
+                "nav-link": true,
+                // Exclamation mark necessary to override
+                // conflicting styling of nav-link class:
+                "!opacity-100": link.href === currentPath,
+              })}
+              href={link.href}
+            >
+              {link.label}
+            </Link>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+const AuthStatus = () => {
+  const { status, data: session } = useSession();
+
+  const userName = session?.user?.name;
+  const userImage = session?.user?.image;
+
+  if (status === "loading") return null;
+
+  if (status === "unauthenticated")
+    return (
+      <Link href="/api/auth/signin" className="nav-link">
+        Sign in
+      </Link>
+    );
+
+  return (
+    <Box>
+      <DropdownMenu.Root>
+        <DropdownMenu.Trigger>
+          <Avatar
+            size="2"
+            radius="full"
+            variant="solid"
+            src={userImage || undefined}
+            fallback={userName ? userName.split(" ")[0][0] : "U"}
+            className="cursor-pointer"
+          />
+        </DropdownMenu.Trigger>
+        <DropdownMenu.Content>
+          <DropdownMenu.Label>
+            <Text>{session!.user!.email}</Text>
+          </DropdownMenu.Label>
+          <DropdownMenu.Separator />
+          <DropdownMenu.Item>
+            <Link href="/api/auth/signout">Sign out</Link>
+          </DropdownMenu.Item>
+        </DropdownMenu.Content>
+      </DropdownMenu.Root>
+    </Box>
   );
 };
 
